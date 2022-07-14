34af2320d781062bf206affb2c8b66798a191d73
chabuei <chabuei@outlook.jp>
2022-07-14 23:18:04 +0900

    Victory message added

--- Main.java
+++ Main.java
@@ -1,22 +1,22 @@
 import java.util.Random;
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
  
     public static void main(String[] args) {
  
         Random random = new Random();
-        //Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int count = 0;
         int count2 = 0;
         int count3 = 0;
         double coin = 0.0;
 
-        /*System.out.println("Who are you??\n");
+        System.out.println("Who are you??\n");
         System.out.println("> ");
         System.out.println("\n");
         String user_name = sc.nextLine();
-        System.out.printf("Hello, " + user_name + "!\n");*/
+        System.out.printf("Hello, " + user_name + "!\n");
         System.out.println("Tossing a coin...\n");
 
         for(int i = 0 ; i < 3 ; i++)
@@ -44,14 +44,14 @@ public class Main {
         }
 
          System.out.printf("Heads: %d, Tails: %d\n", count2, count3);
-         /*if(count2 > count3)
+         if(count2 > count3)
          {
             System.out.printf(user_name + " won!");
          }
          else
          {
             System.out.printf(user_name + " lost!");
-         }*/
+         }
             
     }
  
