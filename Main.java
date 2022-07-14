49e087c8fb6d778aabefd6cac1a4d4d046ab54f0
chabuei <chabuei@outlook.jp>
2022-07-14 23:12:44 +0900

    Hot: initial revision

---
+++ Main.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+//import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+        Random random = new Random();
+        //Scanner sc = new Scanner(System.in);
+        int count = 0;
+        int count2 = 0;
+        int count3 = 0;
+        double coin = 0.0;
+
+        /*System.out.println("Who are you??\n");
+        System.out.println("> ");
+        System.out.println("\n");
+        String user_name = sc.nextLine();
+        System.out.printf("Hello, " + user_name + "!\n");*/
+        System.out.println("Tossing a coin...\n");
+
+        for(int i = 0 ; i < 3 ; i++)
+        {
+            coin = random.nextInt();
+
+            if(coin % 2.0 == 0.0)
+            {
+                count++;
+
+                System.out.printf("Round %d: Heads\n", count);
+
+                count2++;
+            }
+            else
+            {
+                count++;
+
+                System.out.printf("Round %d: Tails\n", count);
+
+                count3++;
+            }
+
+            //System.out.printf("%f", coin); 
+        }
+
+         System.out.printf("Heads: %d, Tails: %d\n", count2, count3);
+         /*if(count2 > count3)
+         {
+            System.out.printf(user_name + " won!");
+         }
+         else
+         {
+            System.out.printf(user_name + " lost!");
+         }*/
+            
+    }
+ 
+}
